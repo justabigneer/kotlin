590a5b977e286dba1893e0ea468a30b80bcc6709
rashi <rashibista456@gmail.com>
2026-02-26 21:05:34 +0545

    Create Strings.kt
    .

---
+++ Strings.kt
@@ -0,0 +1,15 @@
+//This program shows some string templetes in kotlin
+fun main()
+{
+    	val text="""
+    	for (c in "foo")
+        		print(c)"""
+    println(text)
+    val i=10
+    println("i=$i")
+    val letters = listOf("a","b","c","d")
+    println("Letters: $letters")
+    val s="abc"
+    println("$s.length is ${s.length}")
+    
+}
